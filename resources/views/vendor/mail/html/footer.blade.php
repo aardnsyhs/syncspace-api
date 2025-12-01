449af5803086a80067393f2396d5018cc3a6ffc0
aardnsyhs <ardiansyahdian755@gmail.com>
2025-12-01 11:47:59 +0700

    style: standardize mail template formatting and configuration

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,12 @@
+@props([])
 <tr>
-<td>
-<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
-</td>
-</tr>
-</table>
-</td>
+    <td>
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td class="content-cell" align="center" style="padding: 32px 40px;">
+                    {{ Illuminate\Mail\Markdown::parse($slot) }}
+                </td>
+            </tr>
+        </table>
+    </td>
 </tr>
