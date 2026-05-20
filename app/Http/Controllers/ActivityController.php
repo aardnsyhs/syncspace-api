62eca1c9cfcaea4d886f4d8473b862bd5bb02aea
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2026-05-20 09:53:01 +0700

    fix: remove team membership check from activity index method

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -11,10 +11,7 @@ class ActivityController extends Controller
 {
   public function index(Request $request, Board $board): AnonymousResourceCollection
   {
-    
-    if (!$board->team->hasMember($request->user())) {
-      abort(403, 'You are not a member of this team.');
-    }
+    $this->authorize('view', $board);
 
     $activities = $board->activities()
       ->with('user')
